8dcf556ec159fb41764efb59dc7fe881925ab8cb
lkh1434 <lkh1434@gmail.com>
2024-08-21 20:47:31 +0900

    add querier

---
+++ app/encoding.go
@@ -0,0 +1,17 @@
+package gaia
+
+import (
+	"github.com/cosmos/cosmos-sdk/std"
+
+	"github.com/cosmos/gaia/v19/app/params"
+)
+
+// MakeEncodingConfig creates an EncodingConfig.
+func MakeEncodingConfig() params.EncodingConfig {
+	encodingConfig := params.MakeEncodingConfig()
+	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	return encodingConfig
+}
